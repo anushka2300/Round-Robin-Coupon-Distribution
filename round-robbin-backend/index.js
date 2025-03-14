2372d0df4e5fc1a0bf57faa87b4a6f584fa164c5
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2025-03-15 01:14:38 +0530

    commit new

--- round-robbin-backend/index.js
+++ round-robbin-backend/index.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 const allowedOrigins = [
     "http://localhost:3000", 
     "https://round-robbin-frontend-2kdp9bgvl-anushkas-projects-3b34f5d3.vercel.app",  
+    "https://round-robbin-frontend-iplcobtt4-anushkas-projects-3b34f5d3.vercel.app"
   ];
   
   
